55323157f8dfe68524c1005810330533db4fd5a2
Alex Rivera <alexriveracruz4@gmail.com>
2021-02-03 23:49:08 +0000

    C program print

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print "Programming is like building a multilingual puzzle"
+ *
+ * return: 0 (succes exit)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
