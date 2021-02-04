dd4cabe6937151f8b64b3556a18789453b1c9567
Alex Rivera <alexriveracruz4@gmail.com>
2021-02-04 02:24:23 +0000

    C program print phrase

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,13 +3,13 @@
 /**
  * main - Print a phrase
  *
- * Return : 1
+ * Return: 1
  */
 int main(void)
 {
-	write(2,
+	write (2,
 	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	      60);
+	       60);
 
-	return(1);
+	return (1);
 }
