fbabb5ebd900767087987a6471aeea3d132ca7e4
Alex Rivera <alexriveracruz4@gmail.com>
2021-02-09 17:13:32 +0000

    function prints n times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+
+/**
+ * print_times_table - function that prints the n times table, starting with 0
+ * @n: any number between 0 and 15
+ *
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	int a, b, c;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (a = 0; a <= n; a++)
+		{
+			_putchar('0');
+			for (b = 1; b <= n; b++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				c = a * b;
+				if (c < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((c % 10) + '0');
+				}
+				else if (c < 100)
+				{
+					_putchar(' ');
+					_putchar((c / 10) + '0');
+					_putchar((c % 10) + '0');
+				}
+				else
+				{
+					_putchar((c / 10) / 10 + '0');
+					_putchar((c / 10) % 10 + '0');
+					_putchar((c % 10) + '0');
+				}
+				if (b == n)
+				{
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
