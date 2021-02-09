e2df15eadb0cae13fcfa3a2b49dca77c8f151bc5
Alex Rivera <alexriveracruz4@gmail.com>
2021-02-09 15:16:01 +0000

    function absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "holberton.c"
+
+/**
+ * _abs -  function that computes the absolute value of an integer
+ * @n: integer to check
+ *
+ * Return: if n is positive or 0 give n, if n is negative five -n
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
