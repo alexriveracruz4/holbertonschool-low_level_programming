6c5ed957475e2d5686d196223d53f3867ac6edbe
Alex Rivera <alexriveracruz4@gmail.com>
2021-02-09 15:26:10 +0000

    function print minutes

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - a function that prints every minute of the day of Jack Bauer,
+ * starting from 00:00 to 23:59
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int a, b;
+
+        for (a = 0; a <= 23; a++)
+	{
+		for (b = 0; b <= 59; b++)
+		{
+			_putchar((a / 10) + '0');
+			_putchar((a % 10) + '0');
+			_putchar(':');
+			_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
