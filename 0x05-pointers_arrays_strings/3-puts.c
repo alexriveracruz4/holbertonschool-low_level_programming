baeb8ee57fc2f26a53a75040ad47ad83251f8ce6
Alex Rivera <alexriveracruz4@gmail.com>
2021-02-16 14:37:29 +0000

    function print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _puts -  function that prints a string, followed by a new line, to stdout
+ * @str: string to be printed
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int d = 0;
+
+	while (str[d] != 0)
+	{
+		_putchar(str[d]);
+		d++;
+	}
+	_putchar('\n');
+}
