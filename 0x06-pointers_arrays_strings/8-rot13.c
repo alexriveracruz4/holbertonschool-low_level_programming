3b50e52eef0cd47d500b305472c236aea059c1fe
Alex Rivera <alexriveracruz4@gmail.com>
2021-02-18 22:29:16 +0000

    function encode rot13

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * rot13 - unction that encodes a string using rot13
+ * @s: string to encode
+ *
+ * Return: pointer to encoded string
+ */
+char *rot13(char *s)
+{
+	char abc[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char cba[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int pos = 0;
+
+	while (s[pos] != 0)
+	{
+		for (i = 0; i < 52; i++)
+		{
+			if (s[pos] == abc[i])
+			{
+				s[pos] == cba[i];
+				break;
+			}
+		}
+		pos++;
+	}
+	return (s);
+}
