8d91315bf8860c17b050960988058980f5a36606
Alex Rivera <alexriveracruz4@gmail.com>
2021-02-18 23:05:28 +0000

    function print number

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -8,13 +8,14 @@
  */
 void print_number(int n)
 {
+	unsigned int num = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		num = -num;
 	}
-	if (n/10 > 0)
+	if (n / 10 > 0)
 	{
 		print_number(n / 10);
 	}
