207f06ef936ed4b512698a0b40f48cf0204afe28
Alex Rivera <alexriveracruz4@gmail.com>
2021-02-28 01:08:09 +0000

    program add numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - program that adds positive numbers
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
+ *
+ * Return: 0 if the arguments are numbers, 0 otherwise
+ */
+int main(int argc, char *argv[])
+{
+	int i, j;
+	int sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (isdigit(argv[i][j]) == 0)
+			{
+				puts("Error");
+				return (1);
+			}
+		}
+	}
+
+	for (i = 1; i < argc; i++)
+		sum += atoi(argv[i]);
+	printf("%d\n", sum);
+	return (0);
+}
