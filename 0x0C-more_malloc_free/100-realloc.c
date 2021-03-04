874eae7508cceb9f394e453ef5358cf499ca82f9
Alex Rivera <alexriveracruz4@gmail.com>
2021-03-04 23:20:43 +0000

    uploading header file

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,6 +13,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *p;
 	unsigned int i;
+	char *copy = ptr;
 
 	if (ptr == NULL)
 	{
@@ -34,7 +35,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size > old_size)
 		new_size = old_size;
 	for (i = 0; i < new_size; i++)
-		p[i] = ptr[i];
+		p[i] = copy[i];
 	free(ptr);
 	return (p);
 }
