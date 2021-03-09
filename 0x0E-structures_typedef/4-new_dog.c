1b5e21d86c9a101d7a0ec10cec17fc761fda9ca6
Alex Rivera <alexriveracruz4@gmail.com>
2021-03-09 02:46:08 +0000

    function create new struct

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+# include <stdlib.h>
+
+/**
+ * new_dog - function that creates a new dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Return: a new structure
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog2;
+
+	if (name == NULL || owner == NULL)
+		return NULL;
+
+	dog2 = malloc(sizeof(dog_t));
+	if (dog2 == NULL)
+		return NULL;
+	dog2->name = name;
+	dog2->age = age;
+	dog2->owner = owner;
+
+	return(dog2);
+}
