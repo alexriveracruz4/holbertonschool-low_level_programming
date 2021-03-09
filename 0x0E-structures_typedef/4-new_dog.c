7530db72f9eeca5be419ecea22026e13ae9c8595
Alex Rivera <alexriveracruz4@gmail.com>
2021-03-09 02:48:44 +0000

    function create new struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,15 +13,15 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog2;
 
-	if (name == NULL || owner == NULL)
+	if (name == NULL || owner == NULL || age < 0)
 		return NULL;
 
 	dog2 = malloc(sizeof(dog_t));
 	if (dog2 == NULL)
-		return NULL;
+		return (NULL);
 	dog2->name = name;
 	dog2->age = age;
 	dog2->owner = owner;
 
-	return(dog2);
+	return (dog2);
 }
