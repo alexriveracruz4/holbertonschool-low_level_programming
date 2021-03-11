b720645d647d05ae0bd7cc9b7eddad256875a655
Alex Rivera <alexriveracruz4@gmail.com>
2021-03-11 01:53:23 +0000

    function select operator

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,6 +1,5 @@
 #include "3-calc.h"
-#include <stdlib.h>
-
+#include <string.h>
 /**
  * get_op_func -  function that selects the correct function to perform
  * the operation asked by the user
@@ -21,9 +20,11 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	i = 0;
-
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
+	while (ops[i].op != NULL)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+			break;
 		i++;
-
+	}
 	return (ops[i].f);
 }
