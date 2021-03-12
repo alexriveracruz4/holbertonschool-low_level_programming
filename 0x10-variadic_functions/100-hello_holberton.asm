bc0a0d965a7f2c5ca0ebe7672b3509361184bca9
Alex Rivera <alexriveracruz4@gmail.com>
2021-03-12 04:28:22 +0000

    64-bit program print

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,18 +1,16 @@
-global main
+section .data
+	text db "Hello, Holberton",10
 
-	section .text
+section .text
+	global main
 
 main:
-	  mov rax, 1        	; write(
-	  mov rdi, 1        	;   STDOUT_FILENO,
-	  mov rsi, msg      	;   "Hello, world!\n",
-	  mov rdx, msglen   	;   sizeof("Hello, world!\n")
-	  syscall           	; );
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, text
+	mov rdx, 17
+	syscall
 
-	  mov rax, 60       	; exit(
-	  mov rdi, 0        	;   EXIT_SUCCESS
-	  syscall           	; );
-
-	section .rodata
-msg:	 db "Hello, Holberton", 10
-msglen:	 equ $ - msg
+	mov rax, 60
+	mov rdi, 0
+	syscall
