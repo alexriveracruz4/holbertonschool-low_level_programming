5bbdd7f5ed87ad39d6a8131707f38f62fe9bed2d
Alex Rivera <alexriveracruz4@gmail.com>
2021-03-23 22:03:00 +0000

    function find loop

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -14,15 +14,24 @@ listint_t *find_listint_loop(listint_t *head)
 	if (head == NULL || head->next == NULL)
 		return (NULL);
 
-	node2 = head;
-	node1 = NULL;
+	node1 = head->next;
+	node2 = (head->next)->next;
 
 	while (node2)
 	{
-		node1 = node2;
-		node2 = node2->next;
-		if (node1 < node2)
-			return (node2);
+		if (node1 == node2)
+		{
+			node1 = head;
+			while (node1 != node2)
+			{
+				node1 = node1->next;
+				node2 = node2->next;
+			}
+			return (node1);
+		}
+		node1 = node1->next;
+		node2 = (node2->next)->next;
+
 	}
 
 	return (NULL);
