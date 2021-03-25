e3febf9a810b5642e20052eea2b8c1adca8a61c9
Alex Rivera <alexriveracruz4@gmail.com>
2021-03-25 22:10:31 +0000

    function flip bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * flip_bits - function flip
+ * @n: number to be flipped
+ * @m: result number
+ *
+ * Return:  the number of bits you would need to flip to get from one number
+ * to another
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int diff = n ^ m;
+	unsigned int b = 0;
+
+	while (diff != 0)
+	{
+		b += 1 & diff;
+		diff >>= 1;
+	}
+
+	return (b);
+}
