412082c88f3da575dbda2d8921c1606c218246a5
Alex Rivera <alexriveracruz4@gmail.com>
2021-03-30 22:59:10 +0000

    function read a text file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/**
+ * read_textfile - function that reads a text file and prints it to the POSIX
+ * standard output
+ * @filename: file to read
+ * @letters: the number of letters it should read and print
+ *
+ * Return: the actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t op, rd, wr;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+		return (0);
+
+	op = open(filename, O_RDONLY);
+	rd = read(op, buf, letters);
+	wr = write(STDOUT_FILENO, buf, rd);
+
+	if (op == -1 || rd == -1 || wr == -1 || wr != rd)
+	{
+		free(buf);
+		return (0);
+	}
+
+	free(buf);
+	close(op);
+	return (wr);
+}
