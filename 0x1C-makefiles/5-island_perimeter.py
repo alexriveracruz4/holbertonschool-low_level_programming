a311836a8a64ccb1a802c24c74dcc162ca9aec88
Alex Rivera <alexriveracruz4@gmail.com>
2021-07-02 20:25:32 +0000

    [ADD] island perimeter function

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -10,12 +10,12 @@ def island_perimeter(grid):
     s = 0
     e = 0
 
-    for i in range(1, h):
-        for j in range(1, w):
+    for i in range(h):
+        for j in range(w):
             if grid[i][j] == 1:
                 s += 1
-                if (grid[i][j - 1] == 1):
+                if (j > 0 and grid[i][j - 1] == 1):
                     e += 1
-                if (grid[i - 1][j] == 1):
+                if (i > 0 and grid[i - 1][j] == 1):
                     e +=1
     return(4 * s - 2 * e)
